1b30a3f1190f65ce84f3e41165ad3053f5cfdd74
Greg W <gcwestover@gmail.com>
2020-10-27 16:15:58 -0400

    Added ascii art, fixed off-by-one bug

--- index.js
+++ index.js
@@ -9,41 +9,42 @@ const rl = readline.createInterface(
     }
 );
 
-const allowedGuesses = 5
+const allowedGuesses = 6
 const allowedLetters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 let word, letters, guessCount, guessed
-let rounds = 1
+let rounds = 0
 let wins = 0
 let losses = 0
 
 const round = () => {
+    // Reset round variables
+    rounds += 1
     word = wordBank[Math.floor(Math.random()*wordBank.length)]
     letters = word.split('')
     guessCount = 0
     guessed = new Set()
-    // Reset round variables
-    console.log(`Welcome to Round ${rounds}! Press Ctrl + C to exit at any time.`)
+    // Display new round text
+    console.log(`\n Welcome to Round ${rounds}! Press Ctrl + C to exit at any time.`)
     console.log(`This session, you have won ${wins===1 ? '1 time': `${wins} times`} and lost ${losses===1 ? '1 time': `${losses} times`}`)
+    // Call main game logic.
     guess()
 }
 
-const guess = () => rl.question('Guess letter: ', (input) => {
+const guess = () => rl.question('\n Guess letter: ', (input) => {
     // Cleanup input. Strip extra characters and limit to lowercase
-    let letter = input[0].toLowerCase()
+    let letter = input ? input[0].toLowerCase() : undefined
     // Only allow valid letters, if not valid restart guess process
-    if (!allowedLetters.includes(letter)) {
-        guess()
-    }
+    if (!allowedLetters.includes(letter)) {guess(); return undefined}
+    if (!letters.includes(letter) && !guessed.has(letter)) {guessCount += 1}
 
+    console.log('\n' + graphic[guessCount] + '\n')
     // Check if too many guesses made, and game is over
     if (guessCount < allowedGuesses) {
         // Check if the player hasn't guessed the letter before and that the letter 
-        if (!guessed.has(letter) && !letters.includes(letter)) {guessCount += 1}
         
         // Regardless, add the letter to the set of guessed letters
         guessed.add(letter)
         
-        console.log('\n' + graphic[guessCount] + '\n')
         // Generate result string to display, ie. '_ e _ _', and display it
         let result = letters.map((letter) => guessed.has(letter) ? letter : '_').join(" ")
         console.log(result)
@@ -52,20 +53,20 @@ const guess = () => rl.question('Guess letter: ', (input) => {
         //  ie. 't e s t'
         if (result === letters.join(" ")) {
             console.log('You win!')
-            rounds += 1
             wins += 1
             round()
             return undefined
         }
         // Display guesses remaining
-        console.log(`${allowedGuesses+1-guessCount} guesses remaining...`)
-        // Debug logging
+        console.log(`${allowedGuesses-guessCount} guesses remaining...`)
+        // Debugging output
         console.log(letter, guessCount, guessed, letters);
         // Recursively call guess() until out of allowed guesses
         guess()
     } else {
         // If out of guesses, display the correct word. Then update win/loss and start a new round.
         console.log(`Game over! The word was ${word}.`)
+        console.log(letter, guessCount, guessed, letters);
         losses += 1
         round()
     }
