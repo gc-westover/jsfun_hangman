18c8f72441661a65ad3872180135ae1ec8f0aff6
Greg W <gcwestover@gmail.com>
2020-10-28 17:06:19 -0400

    Revised commented version

--- index.js
+++ index.js
@@ -11,36 +11,38 @@ const rl = readline.createInterface(
 
 const allowedGuesses = 6
 const allowedLetters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-let word, letters, guessCount, guessed
+let word, letters, guessCount, guessed, result
 let rounds = 0
 let wins = 0
 let losses = 0
 
 const round = () => {
     rounds += 1
+
     word = wordBank[Math.floor(Math.random()*wordBank.length)]
     letters = word.split('')
     guessCount = 0
     guessed = new Set()
-    // Display new round text
+    result = letters.map((guessedLetter) => guessed.has(guessedLetter) ? guessedLetter : '_').join(" ")
+
     console.log(`\n Welcome to Round ${rounds}! Press Ctrl + C to exit at any time.`)
     console.log(`This session, you have won ${wins===1 ? '1 time': `${wins} times`} and lost ${losses===1 ? '1 time': `${losses} times`}`)
-
+    console.log(result)
+    
     guess()
 }
 
 const guess = () => rl.question('\n Guess letter: ', (input) => {
-    let letter = input ? input[0].toLowerCase() : undefined
-    if (!allowedLetters.includes(letter)) {guess(); return undefined}
-    if (!letters.includes(letter) && !guessed.has(letter)) {guessCount += 1}
-    guessed.add(letter)
-
+    let guessedLetter = input ? input[0].toLowerCase() : undefined
+    if (!allowedLetters.includes(guessedLetter)) {guess(); return undefined}
+    if (!letters.includes(guessedLetter) && !guessed.has(guessedLetter)) {guessCount += 1}
+    guessed.add(guessedLetter)
+    
     console.log('\n' + graphic[guessCount] + '\n')
-
+    
     if (guessCount < allowedGuesses) {
-        let result = letters.map((letter) => guessed.has(letter) ? letter : '_').join(" ")
+        result = letters.map((guessedLetter) => guessed.has(guessedLetter) ? guessedLetter : '_').join(" ")
         console.log(result)
-
         if (result === letters.join(" ")) {
             console.log('You win!')
             wins += 1
--- index_commented.js
+++ index_commented.js
@@ -2,6 +2,7 @@ const readline = require('readline')
 const wordBank = require('./word-bank.json')
 const graphic = require('./ascii.json')
 
+// Initialize interface for IO stream
 const rl = readline.createInterface(
     {
         input: process.stdin,
@@ -9,67 +10,69 @@ const rl = readline.createInterface(
     }
 );
 
+// Initialize game constants
 const allowedGuesses = 6
 const allowedLetters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-let word, letters, guessCount, guessed
+// Declare game global game variables
+let word, letters, guessCount, guessed, result
 let rounds = 0
 let wins = 0
 let losses = 0
 
 const round = () => {
-    // Reset round variables
+    // Increment the round count
     rounds += 1
+
+    // Re-assign all round variables.
     word = wordBank[Math.floor(Math.random()*wordBank.length)]
     letters = word.split('')
     guessCount = 0
     guessed = new Set()
-    // Display new round text
+    result = letters.map((guessedLetter) => guessed.has(guessedLetter) ? guessedLetter : '_').join(" ")
+
+    // Display new round UI
     console.log(`\n Welcome to Round ${rounds}! Press Ctrl + C to exit at any time.`)
     console.log(`This session, you have won ${wins===1 ? '1 time': `${wins} times`} and lost ${losses===1 ? '1 time': `${losses} times`}`)
-    // Call main game logic.
+    console.log(result)
+    
+    // Begin new guess loop
     guess()
 }
 
 const guess = () => rl.question('\n Guess letter: ', (input) => {
-    // Cleanup input. Strip extra characters and limit to lowercase
-    let letter = input ? input[0].toLowerCase() : undefined
-    // Only allow valid letters, if not valid restart guess process
-    if (!allowedLetters.includes(letter)) {guess(); return undefined}
-    if (!letters.includes(letter) && !guessed.has(letter)) {guessCount += 1}
-
+    // Sanitize user input. If string exists, strip out first letter and set to lowercase. If no string, assign guest as undefined.
+    let guessedLetter = input ? input[0].toLowerCase() : undefined
+    // If the input isn't valid, skip to a new guess. Return undefined to end prior function call.
+    if (!allowedLetters.includes(guessedLetter)) {guess(); return undefined}
+    // If the guessed letter is not in the word, and the guessed letter has not been guessed before, increment the number of guesses used.
+    if (!letters.includes(guessedLetter) && !guessed.has(guessedLetter)) {guessCount += 1}
+    // Add the guessed character to the record of guesses.
+    guessed.add(guessedLetter)
+    // Display the guess count.
     console.log('\n' + graphic[guessCount] + '\n')
-    // Check if too many guesses made, and game is over
+    
+    // Check if game is lost!
     if (guessCount < allowedGuesses) {
-        // Check if the player hasn't guessed the letter before and that the letter 
-        
-        // Regardless, add the letter to the set of guessed letters
-        guessed.add(letter)
-        
-        // Generate result string to display, ie. '_ e _ _', and display it
-        let result = letters.map((letter) => guessed.has(letter) ? letter : '_').join(" ")
+        // If game continues, show current game state in form like "_ _ e _"
+        result = letters.map((guessedLetter) => guessed.has(guessedLetter) ? guessedLetter : '_').join(" ")
         console.log(result)
-
-        // Instead of checking if all letters have been guessed, check if result string is in a winning state
-        //  ie. 't e s t'
+        // Instead of checking if the set of guesses letters contains all of the letters in the target word
+        // We check that the game state matches the intended end state, ie "t e s t" == "t e s t"
         if (result === letters.join(" ")) {
+            // If it matches, you win! Tally the win, start a new round, and return undefined to end the current function call.
             console.log('You win!')
             wins += 1
             round()
             return undefined
         }
-        // Display guesses remaining
-        console.log(`${allowedGuesses-guessCount} guesses remaining...`)
-        // Debugging output
-        console.log(letter, guessCount, guessed, letters);
-        // Recursively call guess() until out of allowed guesses
-        guess()
+
     } else {
-        // If out of guesses, display the correct word. Then update win/loss and start a new round.
         console.log(`Game over! The word was ${word}.`)
-        console.log(letter, guessCount, guessed, letters);
         losses += 1
         round()
     }
+    console.log(`${allowedGuesses-guessCount} guesses remaining...`)
+    guess()
 });
 
 round()
